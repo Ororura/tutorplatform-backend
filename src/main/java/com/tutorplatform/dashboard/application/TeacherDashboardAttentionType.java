d292baef45c6516d014c29e4ffb9d83eb89f16bf
Ororura <hitmanlol890@gmail.com>
2026-09-24 12:06:13 +0300

    feat(dashboard): add teacher dashboard read model

---
+++ src/main/java/com/tutorplatform/dashboard/application/TeacherDashboardAttentionType.java
@@ -0,0 +1,7 @@
+package com.tutorplatform.dashboard.application;
+
+public enum TeacherDashboardAttentionType {
+    SUBMISSION_NEEDS_REVIEW,
+    HOMEWORK_OVERDUE,
+    LEARNING_PERIOD_REPORT_MISSING
+}
